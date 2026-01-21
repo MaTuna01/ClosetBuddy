d257e7ef2768ee1a444c7655448274dd723f21d1
tuna0317 <tuna0317@smail.kongju.ac.kr>
2026-01-21 17:10:38 +0900

    - [#122] 미사용 import문 제거

--- main-service/src/main/java/io/codebuddy/closetbuddy/domain/stores/model/dto/UpdateStoreRequest.java
+++ main-service/src/main/java/io/codebuddy/closetbuddy/domain/stores/model/dto/UpdateStoreRequest.java
@@ -1,7 +1,6 @@
 package io.codebuddy.closetbuddy.domain.stores.model.dto;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 public record UpdateStoreRequest(
