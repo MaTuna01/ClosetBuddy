9da713c5233db44b01d5933d91fef2467d3c7580
Chanyoung_KNU <144310780+Machanyoung@users.noreply.github.com>
2026-01-21 09:43:29 +0900

    주석 설명 업데이트

--- main-service/src/main/java/io/codebuddy/closetbuddy/domain/common/config/SecurityConfig.java
+++ main-service/src/main/java/io/codebuddy/closetbuddy/domain/common/config/SecurityConfig.java
@@ -14,15 +14,15 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                // 1. CSRF 보안 끄기 (API 서버는 보통 끔)
+                //CSRF 보안 끄기
                 .csrf(AbstractHttpConfigurer::disable)
 
-                // 2. 모든 요청에 대해 인증 없이 통과 허용 (Gateway가 인증했으니까 믿음)
+                //모든 요청에 대해 인증 없이 통과 허용 (Gateway가 인증을 신뢰)
                 .authorizeHttpRequests(auth -> auth
                         .anyRequest().permitAll()
                 )
 
-                // 3. 로그인 폼, HTTP Basic 인증 끄기 (이게 HTML 응답의 주범임)
+                //로그인 폼, HTTP Basic 인증 무시
                 .formLogin(AbstractHttpConfigurer::disable)
                 .httpBasic(AbstractHttpConfigurer::disable);
 
