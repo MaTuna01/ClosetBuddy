fcb41070db22a80c00c06cc5d8753b0d9d151734
JaeHyun-Son <113869840+sonjh1003@users.noreply.github.com>
2026-01-22 02:08:29 +0900

    [#132] UserService 테스트 코드 구현 및 테스트 통과

--- user-service/src/main/java/io/codebuddy/userservice/domain/common/model/dto/LoginReqDTO.java
+++ user-service/src/main/java/io/codebuddy/userservice/domain/common/model/dto/LoginReqDTO.java
@@ -1,9 +1,13 @@
 package io.codebuddy.userservice.domain.common.model.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class LoginReqDTO {
 
     @NotBlank(message = "아이디를 입력해주세요.")
--- user-service/src/test/java/io/codebuddy/userservice/UserServiceApplicationTests.java
+++ user-service/src/test/java/io/codebuddy/userservice/UserServiceApplicationTests.java
@@ -2,6 +2,7 @@ package io.codebuddy.userservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
 class UserServiceApplicationTests {
