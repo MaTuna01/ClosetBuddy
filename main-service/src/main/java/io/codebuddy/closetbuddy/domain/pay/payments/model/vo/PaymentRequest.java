804d742544fdd1b0ea2988d2a939f7bae2bcb3ff
SongEunKyung <108480766+siwol10@users.noreply.github.com>
2026-01-29 14:49:44 +0900

    payments 미사용 import 정리

--- main-service/src/main/java/io/codebuddy/closetbuddy/domain/pay/payments/model/vo/PaymentRequest.java
+++ main-service/src/main/java/io/codebuddy/closetbuddy/domain/pay/payments/model/vo/PaymentRequest.java
@@ -1,9 +1,7 @@
 package io.codebuddy.closetbuddy.domain.pay.payments.model.vo;
 
 import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Positive;
 
 public record PaymentRequest(
         @NotNull(message = "결제할 주문 번호는 필수 입력 값입니다.")
