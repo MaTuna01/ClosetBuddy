3eb4f8ac8c26b5c80e0dce2d339446d99f215c96
rksdu <rlaskdud444@gmail.com>
2026-02-02 09:13:16 +0900

    [#162] Feign Client 적용 Cart 도메인
    - MSA 구조 변경에 맞춰 Feign Client를 도입하고 장바구니 생성에 대한 Test 코드를 작성하였습니다.

--- main-service/src/main/java/io/codebuddy/closetbuddy/domain/carts/repository/CartItemRepository.java
+++ main-service/src/main/java/io/codebuddy/closetbuddy/domain/carts/repository/CartItemRepository.java
@@ -1,9 +1,13 @@
 package io.codebuddy.closetbuddy.domain.carts.repository;
 
 import io.codebuddy.closetbuddy.domain.carts.model.entity.CartItem;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CartItemRepository extends JpaRepository<CartItem, Long> {
+    Optional<CartItem> findByCartIdAndProductId(Long cartId, Long productId);
 }
