2e1452d9af55a9e3955a66dea46125bc48fa73be
rksdu <rlaskdud444@gmail.com>
2026-02-08 20:30:55 +0900

    [#188] 삭제용 검증 어노테이션 DTO 메시지 수정

--- order-service/src/main/java/io/codebuddy/closetbuddy/domain/carts/model/dto/request/CartDeleteRequest.java
+++ order-service/src/main/java/io/codebuddy/closetbuddy/domain/carts/model/dto/request/CartDeleteRequest.java
@@ -1,13 +1,12 @@
 package io.codebuddy.closetbuddy.domain.carts.model.dto.request;
 
-import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotEmpty;
 
 import java.util.List;
 
 // cartItemId를 삭제하기 위한 Dto
 public record CartDeleteRequest(
-        @NotEmpty(message = "삭제할 상품 목록은 필수입니다.")
+        @NotEmpty(message = "삭제할 장바구니 목록이 없습니다.")
         List<Long> cartItemList
 ) {
 }
