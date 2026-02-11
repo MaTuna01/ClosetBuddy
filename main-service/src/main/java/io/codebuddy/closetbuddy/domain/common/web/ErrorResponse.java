1604ed2147344f19bd73a0e783c983c685b85043
tuna0317 <tuna0317@smail.kongju.ac.kr>
2026-02-11 10:08:03 +0900

    [#200] 중복 에러 response 클래스 삭제

--- main-service/src/main/java/io/codebuddy/closetbuddy/domain/common/web/ErrorResponse.java
+++
@@ -1,8 +0,0 @@
-package io.codebuddy.closetbuddy.domain.common.web;
-
-import java.time.Instant;
-
-public record ErrorResponse(
-        String code,
-        String message,
-        Instant timestamp) { }
