ea3c8e17ebd5562982a88a1c4a67559c7ad83c28
SongEunKyung <108480766+siwol10@users.noreply.github.com>
2026-02-11 20:36:17 +0900

    [#203] Feign Client 설정

---
+++ pay-service/src/main/java/io/codebuddy/payservice/domain/common/feign/config/FeignConfig.java
@@ -0,0 +1,21 @@
+package io.codebuddy.payservice.domain.common.feign.config;
+
+import feign.codec.ErrorDecoder;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Feign Client 설정
+ */
+@Slf4j
+@Configuration
+public class FeignConfig {
+
+    @Bean
+    public ErrorDecoder errorDecoder() {
+        return new FeignErrorDecoder();
+    }
+
+
+}
