43dc4dc84cc49cbf1f4b8e64a1fe1eea621dab14
SongEunKyung <108480766+siwol10@users.noreply.github.com>
2026-02-11 20:37:10 +0900

    [#203] main-service 에서 settlement 제거

--- main-service/src/main/java/io/codebuddy/closetbuddy/domain/settlement/repository/SettlementDetailRepository.java
+++
@@ -1,7 +0,0 @@
-package io.codebuddy.closetbuddy.domain.settlement.repository;
-
-import io.codebuddy.closetbuddy.domain.settlement.model.entity.SettlementDetail;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SettlementDetailRepository extends JpaRepository<SettlementDetail, Long> {
-}
