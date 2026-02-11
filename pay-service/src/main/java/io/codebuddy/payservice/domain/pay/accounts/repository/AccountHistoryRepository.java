c3d64c45d182297408559499abd4c30ef5418b9f
SongEunKyung <108480766+siwol10@users.noreply.github.com>
2026-02-11 19:31:23 +0900

    [#203] accounts repository 추가

---
+++ pay-service/src/main/java/io/codebuddy/payservice/domain/pay/accounts/repository/AccountHistoryRepository.java
@@ -0,0 +1,30 @@
+package io.codebuddy.payservice.domain.pay.accounts.repository;
+
+
+import io.codebuddy.payservice.domain.pay.accounts.model.entity.Account;
+import io.codebuddy.payservice.domain.pay.accounts.model.entity.AccountHistory;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface AccountHistoryRepository extends JpaRepository<AccountHistory,Long> {
+    /*
+      특정 회원(memberId)의 소유인 계좌 내역 전체를 생성일시 내림차순(최신순)으로 조회
+      AccountHistory -> Account -> MemberId 조회
+      @param memberId 조회하려는 회원의 식별자 (로그인된 사용자 본인)
+      @return 해당 회원의 모든 계좌 내역 리스트
+     */
+
+    List<AccountHistory> findByAccount_MemberIdOrderByCreatedAtDesc(Long memberId);
+
+    /*
+    특정 내역(historyId)을 조회하되, 반드시 해당 회원의 계좌(memberId)의 내역인지 함께 검증합니다.
+    @param memberId 요청한 회원의 식별자 (본인 확인용)
+    @param accountHistoryId 조회하려는 내역의 ID
+    @return 두 조건(본인 소유 + 내역 ID)이 모두 일치하는 내역
+     */
+    Optional<AccountHistory> findByAccount_MemberIdAndAccountHistoryId(Long memberId, Long accountHistoryId);
+
+}
+
