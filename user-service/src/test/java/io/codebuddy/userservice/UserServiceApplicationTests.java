f2662b4ffbfc19a231769c3a4d4c254ce0a9d185
tuna0317 <tuna0317@smail.kongju.ac.kr>
2026-02-12 10:58:13 +0900

    [#208] Exception 예외 발생 메서드 수정

--- user-service/src/test/java/io/codebuddy/userservice/UserServiceApplicationTests.java
+++ user-service/src/test/java/io/codebuddy/userservice/UserServiceApplicationTests.java
@@ -2,7 +2,6 @@ package io.codebuddy.userservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
 class UserServiceApplicationTests {
