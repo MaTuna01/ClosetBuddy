80d930a6df71bba1779fa0b67b813a8f945fbe67
rksdu <rlaskdud444@gmail.com>
2026-02-14 20:46:40 +0900

    [#231] - 테스트 DB 반영이 되지 않아 발생한 문제 해결
    해당 어노테이션을 @Disabled 처리하였습니다

--- order-service/src/test/java/io/codebuddy/closetbuddy/OrderServiceApplicationTests.java
+++ order-service/src/test/java/io/codebuddy/closetbuddy/OrderServiceApplicationTests.java
@@ -1,8 +1,10 @@
 package io.codebuddy.closetbuddy;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@Disabled // DB 설정 없이 단위 테스트를 위해 전체 컨텍스트 로드를 제외합니다.
 @SpringBootTest
 class OrderServiceApplicationTests {
 
