f1c569c574889ccb3f5beee7f1818385cb1b9b43
rksdu <rlaskdud444@gmail.com>
2026-02-16 16:14:50 +0900

    [#231] - @Disabled 해제해줍니다

--- order-service/src/test/java/io/codebuddy/closetbuddy/OrderServiceApplicationTests.java
+++ order-service/src/test/java/io/codebuddy/closetbuddy/OrderServiceApplicationTests.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@Disabled // DB 설정 없이 단위 테스트를 위해 전체 컨텍스트 로드를 제외합니다.
 @SpringBootTest
 class OrderServiceApplicationTests {
 
