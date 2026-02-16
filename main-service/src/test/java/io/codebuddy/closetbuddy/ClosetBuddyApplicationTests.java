12cbd29a6b7e173a03a318e6d7a1801932bd294a
tuna0317 <tuna0317@smail.kongju.ac.kr>
2026-02-16 12:10:36 +0900

    [#229] 통합테스트시 외부 인프라 환경을 띄워 테스트하기 위한 container를 활용해 컨테이너 및 환경변수 추가

--- main-service/src/test/java/io/codebuddy/closetbuddy/ClosetBuddyApplicationTests.java
+++ main-service/src/test/java/io/codebuddy/closetbuddy/ClosetBuddyApplicationTests.java
@@ -2,12 +2,57 @@ package io.codebuddy.closetbuddy;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.test.context.ActiveProfiles;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.elasticsearch.ElasticsearchContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.containers.KafkaContainer;
+import org.testcontainers.utility.DockerImageName;
 
 @SpringBootTest
+@Testcontainers
+@ActiveProfiles("test")
 class ClosetBuddyApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+        // mysql 컨테이너
+        @Container
+        @ServiceConnection
+        static MySQLContainer<?> mysql = new MySQLContainer<>("mysql:8.0");
+
+        // Redis 컨테이너
+        @Container
+        @ServiceConnection
+        static GenericContainer<?> redis = new GenericContainer<>(DockerImageName.parse("redis:latest"))
+                        .withExposedPorts(6379);
+
+        // kafka 컨테이너
+        @Container
+        @ServiceConnection
+        static KafkaContainer kafka = new KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka:7.4.4"));
+
+        // els
+        @Container
+        @ServiceConnection
+        // 형태소 분석이미지 그대로 테스트에 활용
+        static ElasticsearchContainer elasticsearch = new ElasticsearchContainer(
+                        DockerImageName.parse("ghcr.io/eddie1031/es:latest")
+                                        .asCompatibleSubstituteFor("docker.elastic.co/elasticsearch/elasticsearch"))
+
+                        // docker compose 파일을 참고하여 개발 환경과 똑같은 환경 구성으로 띄움
+                        // 단일 노드 모드 설정
+                        .withEnv("discovery.type", "single-node")
+                        // 보안 기능 비활성화
+                        .withEnv("xpack.security.enabled", "false")
+                        // ssl 인증 비활성화
+                        .withEnv("xpack.security.http.ssl.enabled", "false")
+                        // jvm 메모리 설정
+                        .withEnv("ES_JAVA_OPTS", "-Xms512m -Xmx512m");
+
+        @Test
+        void contextLoads() {
+        }
 
 }
