706c4f3163b1f449eaf59c0f32281658422a8bca
rksdu <rlaskdud444@gmail.com>
2026-02-17 18:42:18 +0900

    [#231] - 필요없는 import 삭제

--- order-service/src/test/java/io/codebuddy/closetbuddy/OrderServiceApplicationTests.java
+++ order-service/src/test/java/io/codebuddy/closetbuddy/OrderServiceApplicationTests.java
@@ -1,6 +1,5 @@
 package io.codebuddy.closetbuddy;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
