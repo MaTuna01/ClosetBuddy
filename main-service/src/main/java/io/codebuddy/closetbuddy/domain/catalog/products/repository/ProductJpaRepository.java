a8e2ed13664a05c38ed58d2a4c7527eed4df9a5e
rksdu <rlaskdud444@gmail.com>
2026-02-21 16:11:31 +0900

    [#235] - 다건 조회를 위한 쿼리문 추가

--- main-service/src/main/java/io/codebuddy/closetbuddy/domain/catalog/products/repository/ProductJpaRepository.java
+++ main-service/src/main/java/io/codebuddy/closetbuddy/domain/catalog/products/repository/ProductJpaRepository.java
@@ -1,7 +1,9 @@
 package io.codebuddy.closetbuddy.domain.catalog.products.repository;
 
+import feign.Param;
 import io.codebuddy.closetbuddy.domain.catalog.products.model.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -14,4 +16,9 @@ public interface ProductJpaRepository extends JpaRepository<Product, Long> {
 
     //판매자가 가진 모든 가게의 상품을 조회하는 메서드
     List<Product> findAllByStoreId(Long storeId);
+
+    // Order-Service 내 추천 시스템에서 사용되는 메서드
+    // Query 문을 통해 여러 개의 productId를 받아 상품 정보를 한꺼번에 조회합니다.
+    @Query("select p from Product p join fetch p.store s where p.productId in :productIds")
+    List<Product> findRecommendProductWithStore(@Param("productIds") List<Long> productIds);
 }
