900f957b19dd8c58a996681ea4996c71cc592b92
tuna0317 <tuna0317@smail.kongju.ac.kr>
2026-02-24 15:15:23 +0900

    [#245] 라이브러리 import 경로 수

--- order-service/src/main/java/io/codebuddy/closetbuddy/domain/recommend/kafka/RecommendEventConsumer.java
+++ order-service/src/main/java/io/codebuddy/closetbuddy/domain/recommend/kafka/RecommendEventConsumer.java
@@ -1,7 +1,7 @@
 package io.codebuddy.closetbuddy.domain.recommend.kafka;
 
 import io.codebuddy.closetbuddy.domain.carts.service.RecommendResultCacheService;
-import io.codebuddy.closetbuddy.recommend.evnet.RecommendResult;
+import io.codebuddy.closetbuddy.recommend.evnet.*;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
