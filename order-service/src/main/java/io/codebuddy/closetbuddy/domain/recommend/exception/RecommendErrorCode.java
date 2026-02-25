d0545c5aaf052afd73915f4dc93a180dd8b5aaf5
rksdu <rlaskdud444@gmail.com>
2026-02-25 09:22:21 +0900

    [#246] - 추천 에러 코드 수정

--- order-service/src/main/java/io/codebuddy/closetbuddy/domain/recommend/exception/RecommendErrorCode.java
+++ order-service/src/main/java/io/codebuddy/closetbuddy/domain/recommend/exception/RecommendErrorCode.java
@@ -11,7 +11,7 @@ public enum RecommendErrorCode {
 
     // 장바구니를 찾을 수 없을 경우(404 Not Found)
     CART_ITEM_NOT_FOUND(HttpStatus.NOT_FOUND, "장바구니 안에 상품이 없습니다."),
-    RECOMMEND_ERROR_CODE(HttpStatus.NOT_FOUND, "추천 상품을 찾을 수 없습니다.");
+    RECOMMEND_NOT_FOUND(HttpStatus.NOT_FOUND, "추천 상품을 찾을 수 없습니다.");
 
     private final HttpStatus status;
     private final String message;
