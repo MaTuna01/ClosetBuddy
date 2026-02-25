726d0b4304a88332780e6b1e50a51695a6de0ace
JaeHyun-Son <113869840+sonjh1003@users.noreply.github.com>
2026-02-25 09:48:39 +0900

    [#256] Oauth2 로그인에서 중복 계좌생성 문제를 해결하도록 코드를 수정

--- user-service/src/main/java/io/codebuddy/userservice/domain/auth/oauth/service/OauthService.java
+++ user-service/src/main/java/io/codebuddy/userservice/domain/auth/oauth/service/OauthService.java
@@ -15,6 +15,7 @@ import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.swing.*;
 import java.util.NoSuchElementException;
 import java.util.Optional;
 
@@ -42,25 +43,38 @@ public class OauthService extends DefaultOAuth2UserService {
             throw new OAuth2AuthenticationException("Email not found from OAuth2 provider");
         }
 
-        // 기존 회원이면 조회, 없으면 생성
-        Member member = memberRepository.findByEmail(email)
-                .orElseGet(() -> memberRepository.save(
-                        Member.builder()
-                                .username(name)
-                                .memberId(email)
-                                .email(email)
-                                .password("OAUTH_USER")
-                                .role(Role.MEMBER)
-                                .build()
-                ));
+        /* 기존의 문제점: orElseGet() 람다 내부에서만 생성되지만 외부에서 구분이 불가능 하다 -> 기존 회원도 매번 계좌 생성 요청*/
+        // 기존 회원 조회
+        Optional<Member> optionalMember = memberRepository.findByEmail(email);
 
+        boolean isNewMember = optionalMember.isEmpty();
+        if (optionalMember.isEmpty()) {
+            // 신규 회원 생성
+            Member newMember = Member.builder()
+                    .username(name)
+                    .memberId(email)
+                    .email(email)
+                    .password("OAUTH_USER")
+                    .role(Role.MEMBER)
+                    .build();
+            memberRepository.save(newMember);
+            optionalMember = Optional.of(newMember);
+        }
+
+        // Optional에서 실제 Member 객체 꺼내기
+        Member member = optionalMember.get();
         MemberDetails memberDetails = new MemberDetails(member, oAuth2User.getAttributes());
 
         // Main-Service로 계좌 생성 요청 (동기 통신)
-        try {
-            payServiceClient.createAccount(new AccountCreateRequest(member.getId()));
-        } catch (Exception e) {
-            throw new OAuth2AuthenticationException("계좌 생성 실패: "+ e.getMessage());
+        // "신규회원일 때만" 계좌 생성
+        if (isNewMember) {
+            try {
+                payServiceClient.createAccount(new AccountCreateRequest(member.getId()));
+                log.info("Account created for new member: {}", member.getId());
+            } catch (Exception e) {
+                log.warn("PayService account creation failed : memberId={}, error={}",
+                        member.getId(), e.getMessage(), e);
+            }
         }
 
         return memberDetails;
