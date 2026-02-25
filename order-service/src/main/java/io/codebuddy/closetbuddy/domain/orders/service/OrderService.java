563448ed802f7f166b11a8e6eedb84218d245e91
rksdu <rlaskdud444@gmail.com>
2026-02-25 22:01:08 +0900

    [#246] - OrderService 예외 처리 위치 수정
    stockItems 생성 직후 바로 empty 체크 -> 항상 예외를 반환함

--- order-service/src/main/java/io/codebuddy/closetbuddy/domain/orders/service/OrderService.java
+++ order-service/src/main/java/io/codebuddy/closetbuddy/domain/orders/service/OrderService.java
@@ -56,10 +56,6 @@ public class OrderService {
         List<OrderItem> orderItems = new ArrayList<>();
         List<StockItem> stockItems = new ArrayList<>();
 
-        if(stockItems.isEmpty()){
-            throw new OrderException(OrderErrorCode.PRODUCT_NOT_FOUND);
-        }
-
         // 요청 받은 Dto의 orderItems 를 꺼내 주문 생성 Dto에 옮겨 담습니다.
         for (OrderItemCreateRequestDto itemDto : requestDto.orderItems()) {
 
@@ -83,6 +79,10 @@ public class OrderService {
                     itemDto.productId(),
                     itemDto.orderCount()
             ));
+        }
+
+        if(stockItems.isEmpty()){
+            throw new OrderException(OrderErrorCode.PRODUCT_NOT_FOUND);
         }
 
         // 새로운 주문 객체에 orderItem을 저장합니다.
