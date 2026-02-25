3fbd6080048544b404235d7bfe06f8e123752276
rksdu <rlaskdud444@gmail.com>
2026-02-25 23:20:26 +0900

    [#246] - 에러코드 수정

--- order-service/src/main/java/io/codebuddy/closetbuddy/domain/recommend/exception/RecommendErrorCode.java
+++ order-service/src/main/java/io/codebuddy/closetbuddy/domain/recommend/exception/RecommendErrorCode.java
@@ -6,9 +6,6 @@ import org.springframework.http.HttpStatus;
 @Getter
 public enum RecommendErrorCode {
 
-    // 사용자의 장바구니가 아닐 경우(403 Forbidden)
-    NOT_OWNER(HttpStatus.FORBIDDEN, "사용자의 장바구니가 아닙니다."),
-
     // 장바구니를 찾을 수 없을 경우(404 Not Found)
     CART_ITEM_NOT_FOUND(HttpStatus.NOT_FOUND, "장바구니 안에 상품이 없습니다."),
     RECOMMEND_NOT_FOUND(HttpStatus.NOT_FOUND, "추천 상품을 찾을 수 없습니다.");
